f6c22003a166e0edae605f342020896e6e9bc62f
pas8 <peterpreneur@gmail.com>
2022-01-25 00:50:57 -0600

    init

--- HashSet/src/hashset/HashSetTest.java
+++ HashSet/src/hashset/HashSetTest.java
@@ -1,5 +1,22 @@
 package hashset;
 
-public class HashSetTest {
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class HashSetTest {
+	@Test
+	public void should_insert_one_node_into_BST_one_level() {
+		// arrange
+
+		// act
+		sut.insert(50, "50");
+
+		// assert
+		assertThat(sut.get(50), is("50"));
+
+	}	
 }
